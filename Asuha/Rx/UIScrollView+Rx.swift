6b367cd43714338e045c953dc00643a6f695ac73
SaitoYuta <kateinoigakukun@gmail.com>
2017-04-09 19:03:26 +0900

    Create UIScrollView+Rx

---
+++ Asuha/Rx/UIScrollView+Rx.swift
@@ -0,0 +1,16 @@
+//
+//  UIScrollView+Rx.swift
+//  myTwi
+//
+//  Created by SaitoYuta on 2017/04/08.
+//  Copyright © 2017年 SaitoYuta. All rights reserved.
+//
+
+import RxSwift
+import UIKit
+
+public extension Reactive where Base: UIScrollView {
+    var contentOffSet: Observable<CGPoint> {
+        return observe(CGPoint.self, "contentOffset").filterNil()
+    }
+}
