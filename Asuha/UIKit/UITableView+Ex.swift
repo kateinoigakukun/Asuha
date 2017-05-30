9f946875ee90aef6e74facc6ead76a6bbc3767d5
SaitoYuta <kateinoigakukun@gmail.com>
2017-05-30 19:11:25 +0900

    Add UITableView cell register extension

--- Asuha/UIKit/UITableView+Ex.swift
+++ Asuha/UIKit/UITableView+Ex.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 public extension UITableView {
-    public func register<T: UITableViewCell>(cellType: T.Type) {
-        let className = cellType.className
-        let nib = UINib(nibName: className, bundle: nil)
+    public func register<T: UITableViewCell>(forClass type: T.Type) {
+        let className: String = type.className
+        register(type, forCellReuseIdentifier: className)
+    }
+
+    public func register<T: UITableViewCell>(forNib type: T.Type) {
+        let className: String = type.className
+        let nib: UINib = UINib(nibName: className, bundle: nil)
         register(nib, forCellReuseIdentifier: className)
     }
 
-    public func register<T: UITableViewCell>(cellTypes: [T.Type]) {
-        cellTypes.forEach { register(cellType: $0) }
+    public func register<T: UITableViewCell>(forClasses types: [T.Type]) {
+        types.forEach { register(forClass: $0) }
     }
 
-    public func dequeueReusableCell<T: UITableViewCell>(with type: T.Type, for indexPath: IndexPath) -> T {
-        return self.dequeueReusableCell(withIdentifier: type.className, for: indexPath) as! T
+    public func register<T: UITableViewCell>(forNibs types: [T.Type]) {
+        types.forEach { register(forNib: $0) }
     }
 }
