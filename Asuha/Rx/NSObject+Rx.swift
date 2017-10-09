78734786900325cea48289a33b51dc6b9067f7d0
Yuta Saito <kateinoigakukun@gmail.com>
2017-10-10 08:59:12 +0900

    fix disposebag namespace

--- Asuha/Rx/NSObject+Rx.swift
+++ Asuha/Rx/NSObject+Rx.swift
@@ -9,24 +9,50 @@
 import Foundation
 import RxSwift
 
-private var disposeBagKey = 0
-public extension Reactive where Base: NSObject {
+fileprivate struct AssociatedKeys {
+    static var disposeBag = "disposeBag"
+}
 
-    public var bag: DisposeBag {
+public protocol HasDisposeBag: class {
+    var disposeBag: DisposeBag { get set }
+}
+
+extension HasDisposeBag {
+
+    private func doLocked(forClosure closure: () -> Void) {
+        objc_sync_enter(self)
+        defer { objc_sync_exit(self) }
+        closure()
+    }
+    public var disposeBag: DisposeBag {
         get {
-            guard let bag = objc_getAssociatedObject(self, &disposeBagKey) as? DisposeBag else {
-                let newDisposeBag = DisposeBag()
-                setDisposeBag(with: newDisposeBag)
-                return newDisposeBag
+            var rxDisposeBag: DisposeBag!
+            doLocked {
+                if let bag = objc_getAssociatedObject(self, &AssociatedKeys.disposeBag) as? DisposeBag {
+                    rxDisposeBag = bag
+                } else {
+                    let newDisposeBag = DisposeBag()
+                    objc_setAssociatedObject(self, &AssociatedKeys.disposeBag, newDisposeBag, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                    rxDisposeBag = newDisposeBag
+                }
             }
-            return bag
+            return rxDisposeBag
         }
         set {
-            setDisposeBag(with: newValue)
+            objc_setAssociatedObject(self, &AssociatedKeys.disposeBag, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
+}
 
-    private func setDisposeBag(with bag: DisposeBag) {
-        objc_setAssociatedObject(self, &disposeBagKey, bag, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+public extension Reactive where Base: HasDisposeBag {
+
+    var bag: DisposeBag {
+        get { return base.disposeBag }
+        set {
+            let mutableBase = base
+            mutableBase.disposeBag = newValue
+        }
     }
 }
+
+extension NSObject: HasDisposeBag {}
