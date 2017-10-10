fd5b51730b4c8c3e78172bb982e6c3d2b104c48c
Yuta Saito <kateinoigakukun@gmail.com>
2017-10-10 09:17:00 +0900

    add UIViewController extension

--- Asuha/Rx/UIViewController+Rx.swift
+++ Asuha/Rx/UIViewController+Rx.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2017年 bangohan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+public extension Reactive where Base: UIViewController {
+    var viewWillAppear: ControlEvent<Void> {
+        let selector = #selector(UIViewController.viewWillAppear)
+        return ControlEvent(events: sentMessage(selector).map { _ in })
+    }
+    var viewDidLoad: ControlEvent<Void> {
+        let selector = #selector(UIViewController.viewDidLoad)
+        return ControlEvent(events: sentMessage(selector).map { _ in })
+    }
+}
